2e8e393ca40654e850d570721185d6ab3024d2c4
Leihan2016 <leihan2016@gmail.com>
2021-01-21 10:43:16 +0800

    2021.01.21

--- 递归/MiGong.java
+++ 递归/MiGong.java
@@ -7,7 +7,8 @@ public class MiGong
 		int width = 8;
 		//定义棋盘 0 为空
 		int[][] board = new int[height][width];
-
+		
+		//完成棋盘边界初始化
 		for(int i = 0; i < height; i++)
 		{
 			board[i][0] = 1;
@@ -21,15 +22,48 @@ public class MiGong
 		board[3][1] = 1;
 		board[3][2] = 1;
 		board[3][3] = 1;
-		//完成棋盘边界初始化
+		
 		disBoard(board, height, width);
 		
-	
+		findWay(board, 1, 1);
+	    disBoard(board, height, width);
 
 	}
 	
-	//出发位置1,1 结束位置7,7
-	public static 
+	//出发位置i,j 结束位置7,7
+	//1 为墙 2 为通 3 为不通
+	// 路径与行走策略有关 走不通 回溯
+	//函数findway 判断点i，j是否走通
+	public static boolean findWay(int[][] board, int i, int j)
+	{ 
+		if(board[6][6] == 2)
+			return true;
+		else
+		{
+			if(board[i][j] == 0)//表示改点没有走过
+			{
+				//策略下-右-上-左
+				board[i][j] = 2;
+				if(findWay(board, i+1, j)) 
+					return true;
+				else if(findWay(board, i, j+1))
+					return true;
+				else if(findWay(board, i-1, j))
+					return true;
+				else if(findWay(board, i, j-1))
+					return true;
+				else
+				{
+					board[i][j] = 3;
+					return false;
+				}
+			}else
+			{
+				return false;
+			}
+		}
+	
+	}
 
 	public static void disBoard(int[][] board, int height, int width)
 	{
